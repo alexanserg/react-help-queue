246116386a0340393e9f637999d2d3847c236a1e
Alexander Sergejev and Tae Lee <alexander@>
2019-09-04 09:53:49 -0700

    This works with HMR.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App(){
+  return (
+    <div>
+    <Header/>
+    <TicketList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList() {
+  return(
+    <Ticket
+      location="3A"
+      names="Thato and Alex"
+      issue="Firebase will not save record!"/>
+  );
+}
+
+export default TicketList;
