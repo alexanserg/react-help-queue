29f9fbe3db8301b5bdb739aa966a9517267f4c78
Alexander Sergejev and Tae Lee <alexander@>
2019-09-04 10:14:34 -0700

    It works with inline CSS style.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
+import MyStyledComponent from "./MyStyledComponent";
 
 function App(){
   return (
     <div>
     <Header/>
     <TicketList/>
+    <MyStyledComponent/>
     </div>
   );
 }
---
+++ src/components/MyStyledComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function MyStyledComponent(props) {
+  var myStyledComponentStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '5px'
+  }
+  var myStyledh1 = {
+    textAlign: "center",
+    fontFamily: "cursive"
+  }
+  var myStyledh2 = {
+    textAlign: "center",
+    fontFamily: "cursive",
+    color: "Red"
+  }
+  return (
+    <div style={myStyledComponentStyles}>
+    <h1 style={myStyledh1}>Hey, I'm a component</h1>
+    <h2 style={myStyledh2}>But there's something different about me...</h2>
+    <h3>Unlike other components you have worked with thus far....</h3>
+    <h4>I also include custom CSS styles!</h4>
+    <p>Pretty cool, right</p>
+    </div>
+  );
+}
+
+export default MyStyledComponent;
